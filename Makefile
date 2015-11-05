75d769f92e381de10d83be86da5cdd9e8337e595
Scott Pakin <pakin@lanl.gov>
2015-11-04 17:47:11 -0700

    Refactored main out of the parser
    The compiler now accepts a filename instead of literal code.

--- Makefile
+++ Makefile
@@ -4,21 +4,21 @@
 GO = go
 PIGEON = pigeon
 
-GO_SOURCES = qc-prolog.go astnodetype_string.go
+GO_SOURCES = qc-prolog.go parser.go astnodetype_string.go
 
 all: qc-prolog
 
 qc-prolog: $(GO_SOURCES)
 	$(GO) build $(GO_SOURCES)
 
-qc-prolog.go: qc-prolog.peg
-	$(PIGEON) qc-prolog.peg > qc-prolog.tmp
-	goimports qc-prolog.tmp | gofmt > qc-prolog.go
-	$(RM) qc-prolog.tmp
+parser.go: parser.peg
+	$(PIGEON) parser.peg > parser.tmp
+	goimports parser.tmp | gofmt > parser.go
+	$(RM) parser.tmp
 
-astnodetype_string.go: qc-prolog.go
+astnodetype_string.go: parser.go
 	$(GO) generate
 
 clean:
-	$(RM) qc-prolog qc-prolog.go qc-prolog.tmp
-	$(RM) astnodetype_string.go
+	$(RM) qc-prolog qc-prolog.tmp
+	$(RM) parser.go astnodetype_string.go
---
+++ qc-prolog.go
@@ -0,0 +1,36 @@
+// This program implements a compiler for Quantum-Computing Prolog.  It accepts
+// a small subset of Prolog and generates weights for a Hamiltonian expression,
+// which can be fed to a quantum annealer such as the D-Wave supercomputer.
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+)
+
+var notify *log.Logger // Help notify the user of warnings and errors.
+
+// BaseName returns a file path with the directory and extension removed.
+func BaseName(filename string) string {
+	return path.Base(strings.TrimSuffix(filename, path.Ext(filename)))
+}
+
+func main() {
+	// Parse the input file into an AST.
+	progName := BaseName(os.Args[0])
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <input.pl>", progName)
+		os.Exit(1)
+	}
+	notify = log.New(os.Stderr, progName+": ", 0)
+	ast, err := ParseFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// For now, we simply output the AST.
+	fmt.Println(ast)
+}
